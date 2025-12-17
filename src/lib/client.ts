5ddeb25fc397c36d31d7bd59cfd40595d0d4c84c
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2025-12-17 12:40:37 +0530

    tanstack setup and basic setup initialised

---
+++ src/lib/client.ts
@@ -0,0 +1,5 @@
+import { treaty } from "@elysiajs/eden";
+import type { app } from "../app/api/[[...slugs]]/route";
+
+// .api to enter /api prefix
+export const api = treaty<app>("localhost:3000").api;
